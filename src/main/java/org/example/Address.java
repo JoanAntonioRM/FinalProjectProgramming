239af20e7e710742413b68ff9845632705e24f77
JoanAntonioRM <joanantonio1207@gmail.com>
2025-12-13 12:22:48 -0500

    Added the Address class

---
+++ src/main/java/org/example/Address.java
@@ -0,0 +1,77 @@
+package org.example;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
+public class Address {
+    private int streetNo;
+    private String street;
+    private String city;
+    private Province province;
+    private String postalCode;
+
+    /**
+     * Checks if a postal code is valid (Valid format: CDCDCD where C is a character and D is a digit)
+     * @param postalCode the postal code to validate
+     * @return true if valid, false if invalid
+     */
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode == null || postalCode.length() != 6) {
+            return false;
+        }
+
+        for (int i = 0; i < 6; i++) {
+            char c = postalCode.charAt(i);
+            if (i % 2 == 0) {
+                if (!Character.isLetter(c)) {
+                    return false;
+                }
+            } else {
+                if (!Character.isDigit(c)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * All arguments constructor with postal code validation
+     * @param streetNo the street number
+     * @param street the street name
+     * @param city the city
+     * @param province the province
+     * @param postalCode the postal code
+     */
+    public Address(int streetNo, String street, String city, Province province, String postalCode) {
+        if (isPostalCodeValid(postalCode)) {
+            this.streetNo = streetNo;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.postalCode = postalCode.toUpperCase();
+        } else {
+            this.streetNo = 0;
+            this.street = null;
+            this.city = null;
+            this.province = null;
+            this.postalCode = null;
+        }
+    }
+
+    /**
+     * Setter for the postal code after validation
+     * @param postalCode the postal code to set
+     */
+    public void setPostalCode(String postalCode) {
+        if (isPostalCodeValid(postalCode)) {
+            this.postalCode = postalCode.toUpperCase();
+        }
+    }
+}
